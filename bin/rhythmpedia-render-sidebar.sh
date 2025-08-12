ae8f3ee330ac55493afd3cc11f056bc1dce619e1
Atsushi Oka <oka.ats@gmail.com>
2025-08-12 15:44:07 +0900

    perform refactoring on bin directory, again

--- bin/rhythmpedia-render-sidebar.sh
+++ bin/rhythmpedia-render-sidebar.sh
@@ -2,5 +2,5 @@
 
 cat _sidebar.conf | xargs yq ea '. as $i ireduce ({}; . *+ $i)' > _sidebar.generated.yml
 echo "**目次**\n\n" > _sidebar.generated.md
-bin/render-toc2 _sidebar.conf >> _sidebar.generated.md
+bin/rhythmpedia-render-toc.py _sidebar.conf >> _sidebar.generated.md
 
